96a2122346b6d54534e0360eaaacf980f881d718
Komal Verma <komalverma28419@gmail.com>
2026-09-19 12:22:31 +0530

    test payment router

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,10 +1,24 @@
 const express = require("express")
+
 const router = express.Router()
-const { createOrder, verifyPayment } = require("../controllers/paymentController")
+
+const {
+    createOrder,
+    verifyPayment
+} = require("../controllers/paymentController")
+
 const protect = require("../middleware/authMiddleware")
 
 
+router.get("/test", (req, res) => {
+    res.json({
+        success: true,
+        message: "Payment router is working"
+    })
+})
+
 router.post("/create-order", protect, createOrder)
+
 router.post("/verify-payment", protect, verifyPayment)
 
 module.exports = router
