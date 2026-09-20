8895bd4ff271afe7f17a34e11ace366b900111c3
Komal Verma <komalverma28419@gmail.com>
2026-09-20 11:13:32 +0530

    some bug fixed

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,25 +1,20 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.resend.com",
-    port: 587,
-    secure: false,
-
-    auth: {
-        user: "resend",
-        pass: process.env.RESEND_API_KEY,
-    },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOTPEmail = async (email, otp) => {
-    const info = await transporter.sendMail({
+    const { data, error } = await resend.emails.send({
         from: process.env.EMAIL_FROM,
         to: email,
         subject: "CloneX Email Verification OTP",
         text: `Your CloneX verification OTP is ${otp}. It is valid for 10 minutes.`,
     });
 
-    return info;
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return data;
 };
 
 module.exports = {
