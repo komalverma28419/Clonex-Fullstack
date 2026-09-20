f58841c700b713c4cec1abe023a5261ff4746cb5
Komal Verma <komalverma28419@gmail.com>
2026-09-20 10:24:36 +0530

    some bug fixed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,12 +7,13 @@ const nodemailer = require("nodemailer")
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
   connectionTimeout: 30000,
   greetingTimeout: 30000,
   socketTimeout: 30000,
